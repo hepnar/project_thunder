ad85572512ed8ca3a0ed6267c4d51da2feb768b9
Hepnar <hepnar.j@seznam.cz>
2016-05-20 10:46:17 +0200

    rozbalil jsem opengl a hraji si s nehe prikladem

---
+++ src/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# -*- encoding: utf-8 -*-
+
+from src.ptge import Ptge
+
+def start():
+    print("INIT")
+    scene = Ptge()
+    scene.draw()
---
+++ thunder.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- encoding: utf-8 -*-
+
+import sys
+from src.main import start
+
+def main(argv):
+    print("Initializing Thunder")
+    start()
+
+if __name__ == "__main__":
+    main(sys.argv)
