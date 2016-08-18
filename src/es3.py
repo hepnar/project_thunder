0d1c961123d3b5850265513339d057738db34d30
Jiri Hepnar <hepnar.j@gmail.com>
2016-08-18 15:03:55 +0200

    nejake examply

---
+++ src/es3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -*- encoding: utf-8 -*-
+
+import sys
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+def main():
+    print("test")
--- src/main.py
+++ src/main.py
@@ -2,8 +2,10 @@
 # -*- encoding: utf-8 -*-
 
 from src.ptge import Ptge
+from src import es3
 
 def start():
     print("INIT")
     scene = Ptge()
     scene.draw()
+    #es3.main()
