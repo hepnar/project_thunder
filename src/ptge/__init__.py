8fdd72f38082913c5ae559186d61426625a6f1d5
Jiri Hepnar <hepnar.j@gmail.com>
2016-08-25 16:12:01 +0200

    jiny zpusob vykresleni(puziji array)

--- src/ptge/__init__.py
+++ src/ptge/__init__.py
@@ -2,9 +2,14 @@
 # -*- encoding: utf-8 -*-
 
 import sys
+import array
+
+from time import sleep
+
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
+
 
 class Ptge():
     ESCAPE = '\033'
@@ -12,122 +17,59 @@ class Ptge():
     # Number of the glut window.
     window = 0
     def __init__(self):
-        # For now we just pass glutInit one empty argument. I wasn't sure what should or could be passed in (tuple, list, ...)
-        # Once I find out the right stuff based on reading the PyOpenGL source, I'll address this.
+        self.vertices = array.array('f', [ -1,-1,1, -1,1,1, 1,1,1, 1,-1,1,\
+                -1,-1,-1, -1,1,-1,  1,1,-1, 1,-1,-1 ] )
+
+        self.colors = array.array('f', [ 0, 0, 0,  1, 0, 0,  1, 1, 0,  0, 1,\
+                0, 0, 0, 1,  1, 0, 1,  1, 1, 1,  0, 1, 1] )
+
+        self.cIndices = array.array('B', [0, 3, 2, 1,  2, 3, 7, 6,  0, 4, 7, \
+                3, 1, 2, 6, 5,  4, 5, 6, 7,  0, 1, 5, 4 ] )
+
+        self.animationAngle = 0.0
+        self.frameRate = 30
+
+        self._gl_init()
+
+    def _gl_init(self):
         glutInit(())
-
-        # Select type of Display mode:
-        #  Double buffer
-        #  RGBA color
-        # Alpha components supported
-        # Depth buffer
-        glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
-
-        # get a 640 x 480 window
+        glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_ALPHA | GLUT_DEPTH)
         glutInitWindowSize(640, 480)
+        glutInitWindowPosition(100, 100)
+        glutCreateWindow("Project Thunder")
 
-        # the window starts at the upper left corner of the screen
-        glutInitWindowPosition(0, 0)
-
-        # Okay, like the C version we retain the window id to use when closing, but for those of you new
-        # to Python (like myself), remember this assignment would make the variable local and not global
-        # if it weren't for the global declaration at the start of main.
-        self.window = glutCreateWindow("Project Thunder")
+        glClearColor (0, 0, 0, 0)
+        glEnable(GL_DEPTH_TEST)
+        glShadeModel(GL_SMOOTH)
 
     def draw(self):
-           # Register the drawing function with glut, BUT in Python land, at least using PyOpenGL, we need to
-        # set the function pointer and invoke a function to actually register the callback, otherwise it
-        # would be very much like the C version of the code.
         glutDisplayFunc(self._draw_scene)
-
-        # Uncomment this line to get full screen.
-        #glutFullScreen()
-
-        # When we are doing nothing, redraw the scene.
-        glutIdleFunc(self._draw_scene)
-
-        # Register the function called when our window is resized.
-        glutReshapeFunc(self._resize_scene)
-
-        # Register the function called when the keyboard is pressed.
-        #glutKeyboardFunc(keyPressed)
-
-        # Initialize our window.
-        self._init_scene(640, 480)
-
-        # Start Event Processing Engine
+        glutIdleFunc(self._idle_scene)
         glutMainLoop()
 
     def _draw_scene(self):
-        # Clear The Screen And The Depth Buffer
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        glLoadIdentity()                    # Reset The View
-
-        glTranslatef(0.0, 0.0, -6.0)
-
-        self._draw_wall()
-
-        #  since this is double buffered, swap the buffers to display what just got drawn.
-        glutSwapBuffers()
-
-    def _draw_wall(self, center=(0.0,0.0,0.0), rotatiton=(0.0,0.0,0.0)):
-        size_x = 4
-        size_y = 4
-        start_x = size_x / 2.0
-        start_y = size_y / 2.0
-        for x in range(size_x):
-            step_x = 0.5 * x
-            for y in range(size_y):
-                step_y = 0.5 * y
-                self._draw_poligon((start_x-step_x, start_y-step_y, 0.0))
-
-    def _draw_poligon(self, center=(0.0,0.0,0.0), rotation=(0.0,0.0,0.0)):
-        """
-            metoda vykresli sestiuhelnik se stredem v center_{x,y,z}
-        """
-        glBegin(GL_POLYGON)                 # Start drawing a polygon
-        # doleva/doprava, nahoru/dolu, dopredu/dozadu
-        glVertex3f(center[0], center[1] + 0.5, center[2])
-        glVertex3f(center[0] + 0.5, center[1] + 0.25, center[2])
-        glVertex3f(center[0] + 0.5, center[1], center[2])
-        glVertex3f(center[0] + 0.5, center[1] - 0.25, center[2])
-        glVertex3f(center[0], center[1] - 0.5, center[2])
-        glVertex3f(center[0] - 0.5, center[1] - 0.25, center[2])
-        glVertex3f(center[0] - 0.5, center[1], center[2])
-        glVertex3f(center[0] - 0.5, center[1] + 0.25, center[2])
-        glEnd()                             # We are done with the polygon
-
-    # A general OpenGL initialization function.  Sets all of the initial parameters.
-    def _init_scene(self, Width, Height):                # We call this right after our OpenGL window is created.
-        glClearColor(0.0, 0.0, 0.0, 0.0)    # This Will Clear The Background Color To Black
-        glClearDepth(1.0)                    # Enables Clearing Of The Depth Buffer
-        glDepthFunc(GL_LESS)                # The Type Of Depth Test To Do
-        glEnable(GL_DEPTH_TEST)                # Enables Depth Testing
-        glShadeModel(GL_SMOOTH)                # Enables Smooth Color Shading
-
-        glMatrixMode(GL_PROJECTION)
-        glLoadIdentity()                    # Reset The Projection Matrix
-                                            # Calculate The Aspect Ratio Of The Window
-        gluPerspective(45.0, float(Width)/float(Height), 0.1, 100.0)
-
-        glMatrixMode(GL_MODELVIEW)
-
-    # The function called when our window is resized (which shouldn't happen if you enable fullscreen, below)
-    def _resize_scene(self, Width, Height):
-        if Height == 0:                        # Prevent A Divide By Zero If The Window Is Too Small
-            Height = 1
-
-        glViewport(0, 0, Width, Height)        # Reset The Current Viewport And Perspective Transformation
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
-        gluPerspective(45.0, float(Width)/float(Height), 0.1, 100.0)
+        glOrtho(-2, 2, -2, 2, -2, 2)
         glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+        glRotatef(self.animationAngle, 1, 1, 1)
+        glEnableClientState(GL_COLOR_ARRAY)
+        glEnableClientState(GL_VERTEX_ARRAY)
+        glColorPointer(3, GL_FLOAT, 0, self.colors.tostring())
+        glVertexPointer(3, GL_FLOAT, 0, self.vertices.tostring())
+        glDrawElements(GL_QUADS, 24, GL_UNSIGNED_BYTE,
+                       self.cIndices.tostring())
+        glDisableClientState(GL_COLOR_ARRAY)
+        glDisableClientState(GL_VERTEX_ARRAY)
+        glutSwapBuffers()
 
+    def _idle_scene(self):
+        self.animationAngle += 2
+        while self.animationAngle > 360:
+            self.animationAngle -= 360
+        sleep(1 / float(self.frameRate))
+        glutPostRedisplay()
 
-    # The function called whenever a key is pressed. Note the use of Python tuples to pass in: (key, x, y)
-    #def keyPressed(*args):
-    #    # If escape is pressed, kill everything.
-    #    if args[0] == ESCAPE:
-    #        glutDestroyWindow(window)
-    #        sys.exit()
 
